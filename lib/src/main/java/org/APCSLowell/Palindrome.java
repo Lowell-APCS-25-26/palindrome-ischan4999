ada6b035d4ab57f79fc4f0d0d51ef7f7d8fba3a1
ischan4999 <ischan@s.sfusd.edu>
2026-01-31 03:45:26 +0000

    check palindrome

--- lib/src/main/java/org/APCSLowell/Palindrome.java
+++ lib/src/main/java/org/APCSLowell/Palindrome.java
@@ -2,11 +2,21 @@ package org.APCSLowell;
 
 public class Palindrome {
     public boolean palindrome(String s) {
-        return false;
+        s=s.toLowerCase();
+        s=onlyLetters(s);
+        return (s.equals(reverse(s)));
     }
     public String reverse(String s) {
-        String n = new String();
-        // your code here
-        return n;
+        String temp = "";
+        for(int i = s.length(); i>0; i--)
+            temp+=s.substring(i-1,i);
+        return temp;
+    }
+    public String onlyLetters(String sString){
+        String temp = "";
+        for(int i = 0; i<sString.length(); i++)
+            if (Character.isLetter(sString.charAt(i)))
+                temp += sString.substring(i,i+1);
+        return temp;
     }
 }
